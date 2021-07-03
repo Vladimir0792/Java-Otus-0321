2cf2a3b3e658669d639d7770133b05f86260c52c
Vladimir0792 <vova19927@mail.ru>
2021-07-03 16:06:26 +0300

    test

---
+++ hw06-2test/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,39 @@
+package homework;
+
+
+import java.util.ArrayList;
+
+/*public class CustomerReverseOrder {
+
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final ArrayList<Customer> arr = new ArrayList<>();
+    public void add(Customer customer) {
+        arr.add(customer);
+    }
+
+    public Customer take() {
+
+        return (Customer) arr.clone();
+        // это "заглушка, чтобы скомилировать"
+    }
+}*/
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CustomerReverseOrder {
+
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final Deque<Customer> stck = new ArrayDeque<>();
+
+    public void add (Customer customer){
+        stck.push(customer);
+    }
+
+    public Customer take(){
+        return stck.pop();
+    }
+}
