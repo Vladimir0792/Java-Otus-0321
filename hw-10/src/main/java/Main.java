193d0e5baa41898928ecc1df0b8a888c83862e62
Vladimir0792 <vova19927@mail.ru>
2021-07-15 18:49:02 +0300

    AOP

---
+++ hw-10/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import ru.otus.calculators.Calculator;
+import ru.otus.calculators.impl.Addition;
+import ru.otus.proxy.IOC;
+
+public class Main {
+    public static void main(String[] args) {
+        final Calculator addition = IOC.getInstance(new Addition());
+
+        addition.calculate(5, 3);
+    }
+}
