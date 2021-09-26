a11e4d29f023f42dea2ae68fa2935d6daf9f020d
Vladimir0792 <72962394+Vladimir0792@users.noreply.github.com>
2021-09-26 19:59:43 +0300

    Update Nominal.java

--- ATM/src/main/java/ATM/Nominal.java
+++ ATM/src/main/java/ATM/Nominal.java
@@ -1,32 +1,36 @@
 package ATM;
 
 import java.util.Set;
+
 import com.google.common.collect.ImmutableSet;
 
-public class Nominal {
-    private  final  static Set<Integer> POSIBLE = ImmutableSet.of(50, 100, 200, 500, 1000, 2000, 5000);
-    private final  int cost;
+public class BanknoteNominal {
+    private final static Set<Integer> POSIBLE_VALS = ImmutableSet.of(50, 100, 200, 500, 1000, 5000);
+    private final int cost;
 
-    public Nominal(int val){
-        if(!POSIBLE.contains(val)){
-            throw new ATM_Exception("Номинал банкноты не определен"+ val){
-            }
-            this.cost = val;
+    public BanknoteNominal(int value) {
+        if( !POSIBLE_VALS.contains(value)){
+            throw new AtmException("Undefined banknote nominal" + value);
         }
+        this.cost = value;
     }
+
     public int getCost(){
         return cost;
     }
-    public  static Set<Integer> getPosible(){
-        return POSIBLE;
+
+    public static Set<Integer> getPossibleVals(){
+        return POSIBLE_VALS;
     }
+
     @Override
-    public boolean equals(Object o){
-        if(this == o)
-            return true;
-        if(o == null || getClass() != o.getClass())
-            return  false;
-        Nominal obj = (Nominal) o;
-        return !(cost != obj.cost);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BanknoteNominal object = (BanknoteNominal) o;
+        return !(cost != object.cost);
     }
 }
+
+
