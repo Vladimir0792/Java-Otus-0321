0dbf93dfb7f89534bcc81f027b2a0f053c966ca0
Vladimir0792 <72962394+Vladimir0792@users.noreply.github.com>
2021-09-26 19:56:15 +0300

    Update atm.java

--- ATM/src/main/java/ATM/atm.java
+++ ATM/src/main/java/ATM/atm.java
@@ -3,35 +3,39 @@ package ATM;
 import java.util.ArrayList;
 import java.util.List;
 
-public class atm implements Staff, User {
-    private final List<BankCell> cell =new ArrayList<>();
-    private final BankManager bankManager = new BankManager();
+public class Atm implements UserAction, StaffAction {
+    private final List<BanknoteCell> cells = new ArrayList<>();
+    private final CellManager cellManger = new CellManager();
 
-    public  int getBalance(){
+    public int getBalance() {
         int result = 0;
-        for(BankCell bankCell : cell){
-            result += bankCell.getBalance();
+        for (BanknoteCell banknoteCell : cells) {
+            result += banknoteCell.getBalance();
         }
         return result;
     }
-    public int getCellCount(){
-        return cell.size();
-    }
-    @Override
-    public void put(Object[] banknotes){
-        final var countCell = bankManager.tryPutcell(Nominal[])banknotes);
-        bankManager.putCell(countCell);
+
+    public int getCellCnt() {
+        return cells.size();
     }
 
     @Override
-    public  Object[] get(int sum){
-        final var countCell = bankManager.tryCell(sum);
+    public void put(Object[] banknotes) {
+        final var cntByCell = cellManger.tryPutToCells((BanknoteNominal[]) banknotes);
+        cellManger.putToCells(cntByCell);
     }
 
     @Override
-    public  void  putCell(BanknoteCell cells){
-        cell.add(cells);
-        cell.sort((var c1, var c2) -> (c2.getNominal().getCost() - c1.getNominal().get.Cost()));
-        bankManager.setCell(cell);
+    public Object[] get(int sum) {
+        final var cntByCell = cellManger.tryGetFromCells(sum);
+        return cellManger.getFromCells(cntByCell);
     }
+
+    @Override
+    public void putCell(BanknoteCell cell) {
+        cells.add(cell);
+        cells.sort((var m1, var m2) -> (m2.getBanknoteNominal().getCost() - m1.getBanknoteNominal().getCost()));
+        cellManger.setCells(cells);
+    }
+}
 }
