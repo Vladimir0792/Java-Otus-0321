199e0e64586cef90e37c4c807a24a994ead75c5f
Vladimir0792 <vova19927@mail.ru>
2021-10-31 19:33:46 +0300

    OTUS Java-03.21

---
+++ hw07-ATM/src/main/java/ATM/Nominal.java
@@ -0,0 +1,36 @@
+package ATM;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Set;
+
+public class BanknoteNominal {
+    private final static Set<Integer> POSIBLE_VALS = ImmutableSet.of(50, 100, 200, 500, 1000, 5000);
+    private final int cost;
+
+    public BanknoteNominal(int value) {
+        if( !POSIBLE_VALS.contains(value)){
+            throw new AtmException("Undefined banknote nominal" + value);
+        }
+        this.cost = value;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public static Set<Integer> getPossibleVals(){
+        return POSIBLE_VALS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BanknoteNominal object = (BanknoteNominal) o;
+        return !(cost != object.cost);
+    }
+}
+
+
