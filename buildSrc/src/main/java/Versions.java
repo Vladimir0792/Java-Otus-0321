e20a7c941aea51dbdf8be8e651775ffd7cd56613
Vladimir <72962394+Vladimir0792@users.noreply.github.com>
2021-11-03 17:30:56 +0300

    Update Versions.java

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,3 +1,6 @@
 public final class Versions {
 	public static final String GUAVA = "28.2-jre";
+	public static final String glassfishJson = "1.1.4";
+	public static final String reflections = "0.9.11";
+	public static final String asm = "9.1";
 }
