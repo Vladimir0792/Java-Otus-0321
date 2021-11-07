c0eb837262e7b57a62343cb36f490cf81c8349a0
Vladimir <72962394+Vladimir0792@users.noreply.github.com>
2021-11-07 17:45:05 +0300

    Update ComplexProcessor.java

--- hw08-Structural patterns/homework/src/main/java/ru/otus/ru/otus/handler/ComplexProcessor.java
+++ hw08-Structural patterns/homework/src/main/java/ru/otus/ru/otus/handler/ComplexProcessor.java
@@ -1,9 +1,9 @@
-package homework.src.main.java.ru.otus.ru.otus.handler;
 
-import homework.src.main.java.ru.otus.ru.otus.listener.Listener;
-import homework.src.main.java.ru.otus.ru.otus.model.Message;
-import homework.src.main.java.ru.otus.ru.otus.processor.Processor;
+  package ru.otus.handler;
 
+import ru.otus.listener.Listener;
+import ru.otus.model.Message;
+import ru.otus.processor.Processor;
 
 import java.util.ArrayList;
 import java.util.List;
