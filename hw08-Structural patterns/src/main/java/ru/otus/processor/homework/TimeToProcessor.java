f150cd3e00af3c0fb0ce00c9f2abb6d8715c7cf4
Vladimir <72962394+Vladimir0792@users.noreply.github.com>
2021-11-08 15:40:11 +0300

    Update TimeToProcessor.java

--- hw08-Structural patterns/src/main/java/ru/otus/processor/homework/TimeToProcessor.java
+++ hw08-Structural patterns/src/main/java/ru/otus/processor/homework/TimeToProcessor.java
@@ -3,34 +3,31 @@ package ru.otus.processor.homework;
 import ru.otus.model.Message;
 import ru.otus.processor.Processor;
 
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.Optional;
 
 public class TimeToProcessor implements Processor {
-    private Time time;
 
+    private Instant instant;
 
-
-    public TimeToProcessor() {
-        this.time = new Time() {
-            @Override
-            public int getSeconds() {
-                return 0;
-            }
-        };
+    public Instant getInstant() {
+        return instant;
     }
 
-
+    public void setInstant(Instant instant) {
+        this.instant = instant;
+    }
 
     @Override
     public Message process(Message message) {
-        if (time.getSeconds() % 2 == 0) {
-            throw new IllegalCallerException("Процесс идет в четную секунду");
+
+        if (Optional.ofNullable(instant).orElse(Clock.systemUTC().instant()).getEpochSecond() % 2 == 0) {
+            throw new DateTimeException("Процесс идет в четную секунду");
         }
+
         return message;
     }
-
-    interface Time {
-            int getSeconds();
-        }
-
 
 }
